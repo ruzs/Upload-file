f8f1c8525f077a155bf01d5503943eee80842037
ruzs <eddie10913@gmail.com>
2022-12-12 12:05:17 +0800

    在頁面顯示上傳的資料資訊

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@
  * 3.搬移檔案
  * 4.顯示檔案列表
  */
-
+include_once "./api/base.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,6 +15,44 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>檔案上傳</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        .list{
+            list-style-type:none;
+            padding: 0;
+            margin:1rem auto;
+            padding-top:1px;
+            width:80%;
+        }
+        .list-item{
+            display: flex;
+            align-items: center;
+            border: 1px solid #aaa;
+            margin-top:-1px ;
+        }
+        .list-item:nth-child(1){
+            text-align: center;
+            background-color: lightgreen;
+        }
+        .list-item div:nth-child(1){
+            width:10%;
+        }
+        .list-item div:nth-child(1) img{
+            width:150px;
+            height:100px;
+        }
+        .list-item div:nth-child(1){
+            width:30%;
+        }
+        .list-item div:nth-child(2){
+            width:30%;
+        }
+        .list-item div:nth-child(3){
+            width:10%;
+        }
+        .list-item div:nth-child(4){
+            width:20%;
+        }
+    </style>
 </head>
 <body>
  <h1 class="header">檔案上傳練習</h1>
@@ -33,6 +71,41 @@ echo "上傳成功";
 </form>
 
 <!----建立一個連結來查看上傳後的圖檔---->  
+<?php
+$files=all('upload');
+if(count($files)>0){
+echo "<ul class='list'>";
+echo "<li class='list-item'>";
+echo "<div>縮圖</div>";
+echo "<div>描述</div>";
+echo "<div>檔名</div>";
+echo "<div>大小</div>";
+echo "<div>類型</div>";
+echo "</li>";
+    foreach($files as $file){
+        echo "<li class='list-item'>";
+            echo "<div>";
+            echo "<img src='./upload/{$file['file_name']}'>";
+            echo "</div>";
+            echo "<div>";
+            echo $file['description'];
+            echo "</div>";
+            echo "<div>";
+            echo $file['file_name'];
+            echo "</div>";
+            echo "<div>";
+            echo $file['size'];
+            echo "</div>";
+            echo "<div>";
+            echo $file['type'];
+            echo "</div>";
+        echo "</li>";
+    }
+echo "</ul>";
+}else{
+    echo "目前尚無上傳資料";
+}
 
+?>
 </body>
 </html>
